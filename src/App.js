4346cf5945f501d4b197ceec01d3e9461689b5f5
hamdink <hamdi.zraier@dundill.com>
2025-04-27 20:37:56 +0100

    Fix routing logic for user roles in App component

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ export default function App() {
 
 
 
-  const routes = currentUser?.role === 'ADMIN' ? routesAdmin : currentUser?.role === 'PARTNER' ? routesPartner : currentUser?.role === 'ASSISTANT_ADMIN' ? routesAssistantAdmin : currentUser?.role === 'SUPER_ADMIN' ? routesSuperAdmin : null
+  const routes = currentUser?.role === 'ADMIN' ? routesAdmin : currentUser?.role === 'PARTNER' ? routesPartner : currentUser?.role === 'ASSISTANT_ADMIN' ? routesAssistantAdmin : currentUser?.role === 'SUPER_ADMIN' ? routesSuperAdmin : routesAdmin
 
   // Cache for the rtl
   useMemo(() => {
